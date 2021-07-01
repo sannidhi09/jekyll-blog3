189b476d4b267f149d2b6ed42470e406caf279b3
Krishna Sannidhi <sannidhikrishna212@gmail.com>
2021-07-01 10:01:58 -0400

    Deploying final chnages

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 ## How to setup the server
 * To Install Jekyll on Mac follow this [link](https://jekyllrb.com/docs/installation/macos/).
 * Run `bundle exec jekyll serve --trace` in root folder server will host on localhost with port number 4002.
+
+
+## How to deploy it as GitHub page
+* Follow the link https://scriptedtea.com/tech/how-to-deploy-jekyll-to-github/
---
+++ assets/js/fetch.js
@@ -0,0 +1,48 @@
+fetch('https://raw.githubusercontent.com/apache/airavata-site/asf-site/source/_data/team.yml')
+    .then(response => response.text())
+    .then(data => {
+        var obj = jsyaml.load( data );
+        console.log(obj);
+        console.log(obj.length);
+
+        var colums = ['name', 'apache_id', 'email', 'organization', 'roles'];
+
+        var parent = document.getElementsByClassName('js-data-load')[0];
+
+        var tbl = document.createElement("table");
+        
+        // Creating head tag
+        var tblHead = document.createElement("thead");
+        for(var i=0;i<1;i++){
+            var row = document.createElement("tr");
+            for(var j=0;j<colums.length;j++){
+                var cell = document.createElement('th');
+                var cellText = document.createTextNode(colums[j].charAt(0).toUpperCase() + colums[j].slice(1));
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+            tblHead.appendChild(row);
+        }
+        tbl.appendChild(tblHead);
+
+        //Creating table body
+        var tblBody = document.createElement("tbody");
+        for(var i=0;i<obj.length;i++){
+            var row = document.createElement('tr');
+            for(var j=0;j<colums.length;j++){
+                var cell = document.createElement('td');
+                var cellText = document.createTextNode(obj[i][colums[j]].replaceAll(' [at] ', '@').replaceAll(' [dot] ','.'));
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+            tblBody.appendChild(row);
+        }
+        tbl.appendChild(tblBody);
+
+        parent.appendChild(tbl);
+
+        var tblBody = document.createElement("tbody");
+    })
+    .catch(function (err) {
+	console.warn('Something went wrong.', err);
+});
